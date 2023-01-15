d5ff6af6b145cfd5188c8a0256d7e0d79c188e14
zprongua <115650795+zprongua@users.noreply.github.com>
2023-01-14 22:49:08 -0500

    homepage done, logo added

--- src/main/java/com/tazedaily/TAZEDaily/Repository/NewsArticleRepository.java
+++ src/main/java/com/tazedaily/TAZEDaily/Repository/NewsArticleRepository.java
@@ -3,9 +3,12 @@ package com.tazedaily.TAZEDaily.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.tazedaily.TAZEDaily.Domain.Genre;
 import com.tazedaily.TAZEDaily.Domain.NewsArticle;
 
 @Repository
 public interface NewsArticleRepository extends JpaRepository<NewsArticle, Long> {
     NewsArticle findArticleById(Long id);
+
+    Iterable<NewsArticle> findArticlesByGenre(Genre genre);
 }
