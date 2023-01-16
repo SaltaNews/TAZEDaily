0f5f38c9cb8ad7191d1f37f7af2257031a773fc3
annafu1 <104175541+annafu1@users.noreply.github.com>
2023-01-16 15:49:45 -0500

    fixed errors

--- tazedaily/src/index.js
+++ tazedaily/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ErrorBoundary from './ErrorBoundary';
 
 import App from './App';
 import Business from './genre/Business';
@@ -12,14 +13,17 @@ import Travel from './genre/Travel';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
+  //<ErrorBoundary>
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-        <Route path="/business" element={<Sports />} />
+        <Route path="/business" element={<Business />} />
       <Route path="/technology" element={<Technology />} />
-      <Route path="/travel" element={<Sports />} />
+      <Route path="/sports" element={<Sports />} />
+      <Route path="/travel" element={<Travel />} />
     </Routes>
   </BrowserRouter>
+  //</ErrorBoundary>
 );
 
 reportWebVitals();
