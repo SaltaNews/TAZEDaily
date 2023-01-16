05c5f824ca56dd1a67ed7650cce59331f3fd5728
annafu1 <104175541+annafu1@users.noreply.github.com>
2023-01-16 16:56:24 -0500

    updated popularArticle category in genre for business and travel

--- tazedaily/src/genre/Travel.js
+++ tazedaily/src/genre/Travel.js
@@ -18,7 +18,7 @@ const Travel = () => {
         <div className="container text-center">
             <Header />
             <h1> Latest news in Travel</h1>
-            <PopulateArticles category="travel" />
+            <PopulateArticles category="TRAVEL" />
         </div>
     )
 }
